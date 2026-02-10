e24fcf60c48ca6da049eee4179e16b943a37f3d1
Megan <122399676+meganag2@users.noreply.github.com>
2026-02-09 17:09:06 -0700

    Add files via upload

--- decode_fsm.v
+++ decode_fsm.v
@@ -1,124 +1,88 @@
 module decode_fsm (
-	input wire clk,
-	input wire reset,
-	input wire [4:0] Flags_out,
-	input wire [15:0] aluBus, 
-	
-	output reg [15:0] wEnable,
-	output reg [15:0] Imm_in,
-	output reg [7:0] opcode,
-	output reg [3:0] Rdest_sel,
-	output reg [3:0] Rsrc_sel,
-	output reg Imm_sel,
-	output reg [15:0] decoder,
-	output reg decoder_en;
+    input  wire        clk,
+    input  wire        reset,     // active-low reset
+
+    // instruction set coming from hard-coded values in top-level module
+    input  wire [15:0]  instr_set;  
+
+    // FSM outputs (match slide names)
+    output reg         pc_en,       // PCe
+    output reg         w_en,         // write enable
+    output reg  [3:0]  rsrc,
+    output reg  [3:0]  rdest,
+    output reg  [7:0]  opcode,
+    output reg         imm_sel,     // R/I
 );
 
-	// STATE ENCODING
-	localparam FETCH = 4'd0;
-	localparam DECODE = 4'd1;
-	localparam EXECUTE = 4'd2;
+    // state encoding
+    localparam S0_FETCH   = 2'd0;
+    localparam S1_DECODE  = 2'd1;
+    localparam S2_EXECUTE = 2'd2;
 
-	// state registers (present state, next state)
-	reg [3:0] PS, NS;
-	reg [15:0] currentInst;
+    reg [1:0] PS, NS;
 
-	
-	// ALU opcodes
-	localparam [7:0] ADDU = 8'b0000_0110;
-	localparam [7:0] ADDUI = 8'b0110_0000;
-	localparam [7:0] CMP = 8'b0000_1011;
-	localparam [7:0] NOP = 8'b0000_0000;
-	
-	localparam [15:0] N_VALUE = 16'd10;
-	
-	// Update state registers on every clock cycle
-	always @(posedge clk or negedge reset) begin
-		if (!reset) // check for reset button
-			PS <= RESET;
-		else
-			PS <= NS;
-	end
-	
-	// Logic for the next state
-	always @(*) begin
-		NS = PS;
-		case (PS)
-			RESET:
-				NS = INIT_B;
-			INIT_B:
-				NS = INIT_N;
-			INIT_N:
-				NS = CHECK;
-			
-			CHECK: begin
-				if (Flags_out[4])
-					NS = ADD_AB;
-				else
-					NS = DONE;
-			end
-			
-			ADD_AB:
-				NS = MOVE_A;
-			MOVE_A:
-				NS = MOVE_B;
-			MOVE_B:
-				NS = INC_I;
-			INC_I:
-				NS = WRITE_OUT;
-			WRITE_OUT:
-				NS = CHECK;
-			
-			DONE:
-				NS = DONE;
-			default:
-				NS = RESET;
-		endcase
-	end
-	
-	// Control logic
-	always @(*) begin
-		// initialize output values
-		wEnable = 16'b0;
-		Imm_in = 16'h0000;
-		opcode = NOP; // waiting
-		Rdest_sel = 4'd0;
-		Rsrc_sel = 4'd0;
-		Imm_sel = 1'b0;
-		
-		// do some logic based on the present state
-		case (PS)
-		
-					// move a to equal b
-			FETCH: begin
-				opcode = ADDUI;
-				decoder_en = 0;
-				Rdest_sel = 4'01;
-				Imm_in = 16'd0;
-				Imm_sel = 1'b1;
-				currentInst = aluIn;
-			end
-			// set b = 1
-			DECODE: begin
-				decoder <= currentInst;
-				Rdest_sel = 4'd1; // r1 (from datapath)
-				Imm_in = 16'd1;
-				Imm_sel = 1'b1; // using immediate
-				wEnable = 16'b0000_0000_0000_0010;
-			end
-			
-			// set N = N_VALUE (some hard coded value for demo)
-			INIT_N: begin
-				opcode = ADDUI;
-				Rdest_sel = 4'd4; // r4 (from datapath)
-				Imm_in = N_VALUE;
-				Imm_sel = 1'b1;
-				wEnable = 16'b0000_0000_0001_0000;
-			end
-			DONE: begin
-				opcode = NOP;
-			end
-		endcase
-	end
-	
+    // State register
+    always @(posedge clk or negedge reset) begin
+        if (!reset)
+            PS <= S0_FETCH;
+        else
+            PS <= NS;
+    end
+
+    // Next-state logic
+    always @(*) begin
+        NS = PS;
+        case (PS)
+            S0_FETCH:   NS = S1_DECODE;
+
+            S1_DECODE: 	NS = S2_EXECUTE; 
+
+            S2_EXECUTE: NS = S0_FETCH;
+
+            default:    NS = S0_FETCH;
+        endcase
+    end
+	 
+	 
+    // Output logic 
+    always @(*) begin
+        // set defaults
+		  pc_en   = 1'b0;
+		  w_en	 = 1'b0;
+        rsrc    = 4'b0000;
+        rdest   = 4'b0000;
+        opcode  = 8'h00;
+        imm_sel = 1'b0;
+
+
+        case (PS)
+            S0_FETCH: begin
+					 // does nothing
+                pc_en   = 1'b0;
+                w_en     = 1'b0;
+            end
+
+            S1_DECODE: begin
+                pc_en   = 1'b0;
+					 
+					 decoder u_decoder(
+						.instr_set(instr_set),
+						.clk(clk),
+						.reset(reset),
+						.wEnable(w_en),
+						.Imm_select(imm_sel)
+						.opcode(opcode),
+						.Rdest(rdest),
+						.Rsrc_Imm(rsrc),
+					);
+
+            end
+
+            S2_EXECUTE: begin
+                pc_en   = 1'b1;          //  PC increments
+					 w_en		= 1'b1;			  // 	write to reg file
+            end
+        endcase
+    end
+
 endmodule
