c55e57f68900f2b05d305e82b67aa36a4624c18a
Aliou Tippett <avt.tippett@gmail.com>
2026-02-17 15:56:53 -0700

    Refactor RegBank module and update reset logic

--- regfile.v
+++ regfile.v
@@ -26,8 +26,8 @@ module Register (
 	 output reg [15:0] r
 );
 	 
- always @( posedge clk or posedge reset) begin
-	 if (reset) r <= 16'h0000;
+ always @( posedge clk or negedge reset) begin
+	if (!reset) r <= 16'h0000;
 	else
 		begin			
 			if (w_Enable)
@@ -50,33 +50,37 @@ endmodule
 
 // Structural Implementation of RegBank
 /********/
-module RegBank(ALUBus, r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, regEnable, clk, reset);
-	input clk, reset;
-	input [15:0] ALUBus;
-	input [15:0] regEnable;
-	output [15:0] r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15;
+module RegBank(
+    input [15:0] ALUBus,       // Data to write to register
+    input [3:0] wEnable,         // 4-bit destination register index
+    input clk, reset,          // Clock and Reset
+    output [15:0] r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15 // All register outputs
+);
 
-	
-Register Inst0(
-	.Result(ALUBus),
-	.w_Enable(regEnable[0]),
-	.reset(reset), 
-	.clk(clk),
-	.r(r0));
-Register Inst1(ALUBus, regEnable[1], reset, clk, r1);
-Register Inst2(ALUBus, regEnable[2], reset, clk, r2);
-Register Inst3(ALUBus, regEnable[3], reset, clk, r3);
-Register Inst4(ALUBus, regEnable[4], reset, clk, r4);
-Register Inst5(ALUBus, regEnable[5], reset, clk, r5);
-Register Inst6(ALUBus, regEnable[6], reset, clk, r6);
-Register Inst7(ALUBus, regEnable[7], reset, clk, r7);
-Register Inst8(ALUBus, regEnable[8], reset, clk, r8);
-Register Inst9(ALUBus, regEnable[9], reset, clk, r9);
-Register Inst10(ALUBus, regEnable[10], reset, clk, r10);
-Register Inst11(ALUBus, regEnable[11], reset, clk, r11);
-Register Inst12(ALUBus, regEnable[12], reset, clk, r12);
-Register Inst13(ALUBus, regEnable[13], reset, clk, r13);
-Register Inst14(ALUBus, regEnable[14], reset, clk, r14);
-Register Inst15(ALUBus, regEnable[15], reset, clk, r15); 
+    // Register Instantiations
+    Register Inst0(.Result(ALUBus), .w_Enable(regEnable[0]), .reset(reset), .clk(clk), .r(r0));
+    Register Inst1(.Result(ALUBus), .w_Enable(regEnable[1]), .reset(reset), .clk(clk), .r(r1));
+    Register Inst2(.Result(ALUBus), .w_Enable(regEnable[2]), .reset(reset), .clk(clk), .r(r2));
+    Register Inst3(.Result(ALUBus), .w_Enable(regEnable[3]), .reset(reset), .clk(clk), .r(r3));
+    Register Inst4(.Result(ALUBus), .w_Enable(regEnable[4]), .reset(reset), .clk(clk), .r(r4));
+    Register Inst5(.Result(ALUBus), .w_Enable(regEnable[5]), .reset(reset), .clk(clk), .r(r5));
+    Register Inst6(.Result(ALUBus), .w_Enable(regEnable[6]), .reset(reset), .clk(clk), .r(r6));
+    Register Inst7(.Result(ALUBus), .w_Enable(regEnable[7]), .reset(reset), .clk(clk), .r(r7));
+    Register Inst8(.Result(ALUBus), .w_Enable(regEnable[8]), .reset(reset), .clk(clk), .r(r8));
+    Register Inst9(.Result(ALUBus), .w_Enable(regEnable[9]), .reset(reset), .clk(clk), .r(r9));
+    Register Inst10(.Result(ALUBus), .w_Enable(regEnable[10]), .reset(reset), .clk(clk), .r(r10));
+    Register Inst11(.Result(ALUBus), .w_Enable(regEnable[11]), .reset(reset), .clk(clk), .r(r11));
+    Register Inst12(.Result(ALUBus), .w_Enable(regEnable[12]), .reset(reset), .clk(clk), .r(r12));
+    Register Inst13(.Result(ALUBus), .w_Enable(regEnable[13]), .reset(reset), .clk(clk), .r(r13));
+    Register Inst14(.Result(ALUBus), .w_Enable(regEnable[14]), .reset(reset), .clk(clk), .r(r14));
+    Register Inst15(.Result(ALUBus), .w_Enable(regEnable[15]), .reset(reset), .clk(clk), .r(r15));
+
+    // Generate the regEnable signal: only one bit should be high
+    reg [15:0] regEnable;
+
+    always @(*) begin
+        regEnable = 16'b0;  // Default to all bits off
+        regEnable[rdest] = 1; // Set the rdest bit high (only one bit should be set)
+    end
 
 endmodule
