87e0c6aef298b37cecb2f6ead5e3fad5e3c36b63
MeshalAlali <mishaalmessi@gmail.com>
2026-02-17 18:51:50 -0700

    Update load_store_TOP.v

--- LAB4b-tbs/load_store_TOP.v
+++ LAB4b-tbs/load_store_TOP.v
@@ -1,179 +1,159 @@
 module load_store_TOP(
-    
-	 input  wire CLOCK_50, 
-    input  wire [3:0]  KEY
-   // output wire [9:0]  LEDR,
-  //  output wire [6:0]  HEX0,
-  //  output wire [6:0]  HEX1,
-  //  output wire [6:0]  HEX2,
-  //  output wire [6:0]  HEX3
-    
+    input  wire        CLOCK_50,
+    input  wire [3:0]  KEY,
+    output wire [9:0]  LEDR,
+    output wire [6:0]  HEX0,
+    output wire [6:0]  HEX1,
+    output wire [6:0]  HEX2,
+    output wire [6:0]  HEX3,
+    output wire [6:0]  HEX4,
+    output wire [6:0]  HEX5
 );
 
-// GLOBAL WIRES
-wire reset = KEY[0];
-wire clk_slow;
+    // KEY[0] on the DE1 is active-low
+    wire reset_n = KEY[0];
 
-/***************************
-			FSM --> DataPath
-***************************/
-/* Ram */
-wire we_a; 
-wire en_a; 
-wire en_b; 
-wire ram_we;
-/* LS_cntr MUX */
-wire lsc_mux_selct;
-/* PC */
-wire [15:0] pc_add_k;
-wire pc_mux_selct;
-wire pc_en;
-/* Reg file */
-wire [15:0] wEnable;
-/* ALU/ Muxes */ 
-wire [15:0] imm_in;
-wire [7:0]  opcode;
-wire [3:0]  Rdest_sel;
-wire [3:0]  Rsrc_sel;
-wire        imm_sel;
-wire fsm_alu_mem_selct;
+    wire clk_slow;
 
-/***************************
-			Data Path --> FSM
-***************************/
-wire [4:0] Flags;
-wire [15:0] ram_out;
-reg [15:0] instr_set;
+    // Clock Divider (active-low reset)
+    clock_div #(.DIV(5_000_000)) u_div (
+        .clk_in (CLOCK_50),
+        .reset  (reset_n),
+        .clk_out(clk_slow)
+    );
 
+    // FSM <-> datapath wires
+    wire [15:0] wEnable;
+    wire [7:0]  opcode;
+    wire [3:0]  Rdest_select;
+    wire [3:0]  Rsrc_select;
+    wire [15:0] Imm_in;
+    wire        Imm_select;
 
+    wire we_a, en_a, en_b, ram_wen;
+    wire lsc_mux_selct;
 
-// FSM
-decode_fsm fsm (
+    wire [15:0] pc_add_k;
+    wire pc_mux_selct, pc_en;
 
-/*global */
- .clk(clk_slow),
- .reset(reset),
-/* PC */
-.pc_add_k(pc_add_k),
-.pc_mux_selct(pc_mux_selct),
-.pc_en(pc_en),
-/* LS_cntr MUX */
-.lsc_mux_selct(lsc_mux_selct),
-/* RAM */
-.ram_we(ram_we),
-.we_a(we_a), 
-.en_a(en_a), 
-.en_b(en_b),
-ram_out(ram_out), // data out ---> FIX ME
-/* ALU */
-.opcode(opcode),
-.Flags_in(Flags),
-/* Reg file */
-.wEnable(wEnable),
+    wire fsm_alu_mem_selct;
+    wire decoder_en;
 
-/* MUXS: */ 
-.fsm_alu_mem_selct(fsm_alu_mem_selct),
-.Rdest_select(Rdest_sel),
-.Rsrc_select(Rsrc_sel),
-.Imm_in(imm_in),
-.Imm_select(imm_sel),
+    wire [15:0] ram_out;
 
-.instr_set(instr_set)
+    wire [15:0] r0;
+    wire [15:0] r1;
+    wire [15:0] r2;
+    wire [15:0] r3;
+    wire [15:0] r4;
+    wire [15:0] r5;
+    wire [15:0] r6;
+    wire [15:0] r7;
+    wire [15:0] r8;
+    wire [15:0] r9;
+    wire [15:0] r10;
+    wire [15:0] r11;
+    wire [15:0] r12;
+    wire [15:0] r13;
+    wire [15:0] r14;
+    wire [15:0] r15;
 
-);
+    wire [4:0] Flags_out;
 
+    wire [15:0] pc_count;
 
-	
-// Data Path
-data_path dp (
+    load_store_FSM fsm(
+        .clk(clk_slow),
+        .reset(reset_n),
 
-/*global */
- .clk(clk_slow),
- .reset(reset),
-/* PC */
-.pc_add_k(pc_add_k),
-.pc_mux_selct(pc_mux_selct),
-/* LS_cntr MUX */
-.lsc_mux_selct(lsc_mux_selct),
-/* RAM */
-.ram_we(ram_we),
-.we_a(we_a), 
-.en_a(en_a), 
-.en_b(en_b),
-ram_out(ram_out), // data out ---> FIX ME
-/* ALU */
-.opcode(opcode),
-.Flags_out(Flags),
-/* Reg file */
-.wEnable(wEnable),
+        .instr_set(ram_out),
+        .Flags_in(Flags_out),
 
-/* MUXS: */ 
-.fsm_alu_mem_selct(fsm_alu_mem_selct),
-.Rdest_select(Rdest_sel),
-.Rsrc_select(Rsrc_sel),
-.Imm_in(imm_in),
-.Imm_select(imm_sel)
-);
+        .wEnable(wEnable),
 
+        .opcode(opcode),
+        .Rdest_select(Rdest_select),
+        .Rsrc_select(Rsrc_select),
+        .Imm_in(Imm_in),
+        .Imm_select(Imm_select),
 
-// Clock Divider
-clock_div #(.DIV(5_000_000)) u_div (
-    .clk_in (CLOCK_50),
-    .reset  (reset),
-    .clk_out(clk_slow)
-);
+        .we_a(we_a),
+        .en_a(en_a),
+        .en_b(en_b),
+        .ram_wen(ram_wen),
 
+        .lsc_mux_selct(lsc_mux_selct),
 
+        .pc_add_k(pc_add_k),
+        .pc_mux_selct(pc_mux_selct),
+        .pc_en(pc_en),
 
-// Instruction ROM
-/*
-always @(posedge clk_slow or negedge reset) begin
-    if (!reset)
-        instr_set <= 16'b0;
-    else begin
-        case (pc_count)
-            3'd0: instr_set <= 16'b0101_0001_0000_0101;
-            3'd1: instr_set <= 16'b0000_0010_0001_1101;
-            3'd2: instr_set <= 16'b0000_0011_0001_0101;
-            3'd3: instr_set <= 16'b0000_0100_0011_1001;
-            default: instr_set <= 16'b0;
-        endcase
-    end
-end
-*/
+        .fsm_alu_mem_selct(fsm_alu_mem_selct),
+        .decoder_en(decoder_en)
+    );
 
-// HEX function
-function [6:0] hex7;
-    input [3:0] x;
-    begin
-        case (x)
-            4'h0: hex7 = 7'b1000000;
-            4'h1: hex7 = 7'b1111001;
-            4'h2: hex7 = 7'b0100100;
-            4'h3: hex7 = 7'b0110000;
-            4'h4: hex7 = 7'b0011001;
-            4'h5: hex7 = 7'b0010010;
-            4'h6: hex7 = 7'b0000010;
-            4'h7: hex7 = 7'b1111000;
-            4'h8: hex7 = 7'b0000000;
-            4'h9: hex7 = 7'b0010000;
-            4'hA: hex7 = 7'b0001000;
-            4'hB: hex7 = 7'b0000011;
-            4'hC: hex7 = 7'b1000110;
-            4'hD: hex7 = 7'b0100001;
-            4'hE: hex7 = 7'b0000110;
-            4'hF: hex7 = 7'b0001110;
-            default: hex7 = 7'b1111111;
-        endcase
-    end
-endfunction
+    data_path #(
+        // NOTE: if Quartus can't find this, change to an absolute path like other labs
+        .DATA_FILE("LAB4b-tbs/load_store.hex")
+    ) dp(
+        .clk(clk_slow),
+        .reset(reset_n),
+        .ram_we(ram_wen),
 
-// The commented-out assignments for HEX could be re-enabled if needed:
- //assign HEX0 = hex7(data_out[3:0]);
-//assign HEX1 = hex7(data_out[3:0]);
- //assign HEX2 = hex7(pc_count[3:0]);
- //assign HEX3 = hex7(pc_count [7:4]);
+        .wEnable(wEnable),
 
-// assign pc_current = pc_count; // Corrected assignment for pc_current
+        .opcode(opcode),
+        .Flags_out(Flags_out),
+
+        .we_a(we_a),
+        .en_a(en_a),
+        .en_b(en_b),
+
+        .lsc_mux_selct(lsc_mux_selct),
+
+        .pc_add_k(pc_add_k),
+        .pc_mux_selct(pc_mux_selct),
+        .pc_en(pc_en),
+
+        .ram_out(ram_out),
+        .pc_count(pc_count),
+
+        .fsm_alu_mem_selct(fsm_alu_mem_selct),
+        .Rdest_select(Rdest_select),
+        .Rsrc_select(Rsrc_select),
+        .Imm_in(Imm_in),
+        .Imm_select(Imm_select),
+
+        .r0(r0),
+        .r1(r1),
+        .r2(r2),
+        .r3(r3),
+        .r4(r4),
+        .r5(r5),
+        .r6(r6),
+        .r7(r7),
+        .r8(r8),
+        .r9(r9),
+        .r10(r10),
+        .r11(r11),
+        .r12(r12),
+        .r13(r13),
+        .r14(r14),
+        .r15(r15)
+    );
+
+    // Simple debug display:
+    //  - HEX1:HEX0 shows r0
+    //  - HEX3:HEX2 shows r2
+    //  - HEX5:HEX4 shows pc_count
+    hex7seg h0(.hex(r0[3:0]),       .seg(HEX0));
+    hex7seg h1(.hex(r0[7:4]),       .seg(HEX1));
+    hex7seg h2(.hex(r2[3:0]),       .seg(HEX2));
+    hex7seg h3(.hex(r2[7:4]),       .seg(HEX3));
+
+    hex7seg h4(.hex(pc_count[3:0]), .seg(HEX4));
+    hex7seg h5(.hex(pc_count[7:4]), .seg(HEX5));
+
+    assign LEDR = r2[9:0];
 
 endmodule
