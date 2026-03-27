21ea750942a97a21d70683a67c85881de6f4c566
mmess1 <mishaalmessi@gmail.com>
2026-03-27 15:32:00 -0600

    Refactor PONG_TOP.v to include potentiometer handling

--- 1_PROJECT_PONG/PONG_TOP.v
+++ 1_PROJECT_PONG/PONG_TOP.v
@@ -26,31 +26,30 @@ module PONG_TOP(
     output wire [7:0] VGA_B
 );
 
+    wire [11:0] pot0_raw;
+    wire [11:0] pot1_raw;
     wire        sample_strobe;
-    wire [8:0]  y_pos1;
-    wire [8:0]  y_pos2;
-    wire [9:0]  ball_x;
-    wire [9:0]  ball_y;
-    wire [3:0]  score1;
-    wire [3:0]  score2;
 
-    localparam [8:0] Y_MIN   = 9'd10;
-    localparam [8:0] Y_MAX   = 9'd415;
-    localparam [8:0] Y_RANGE = Y_MAX - Y_MIN;
+    wire [8:0] y_pos1;
+    wire [8:0] y_pos2;
+    wire [9:0] ball_x;
+    wire [9:0] ball_y;
+    wire [3:0] score1;
+    wire [3:0] score2;
 
-    wire [2:0] p1_level;
-    wire [2:0] p2_level;
+    wire [9:0] pot0_dec;
+    wire [9:0] pot1_dec;
+
+    wire [3:0] pot0_ones;
+    wire [3:0] pot0_tens;
+    wire [3:0] pot0_hundreds;
+
+    wire [3:0] pot1_ones;
+    wire [3:0] pot1_tens;
+    wire [3:0] pot1_hundreds;
 
     assign VGA_SYNC_N = 1'b0;
     assign LEDR       = 10'b0;
-
-    assign p1_level = (y_pos1 <= Y_MIN) ? 3'd0 :
-                      (y_pos1 >= Y_MAX) ? 3'd6 :
-                      (((y_pos1 - Y_MIN) * 6) / Y_RANGE);
-
-    assign p2_level = (y_pos2 <= Y_MIN) ? 3'd0 :
-                      (y_pos2 >= Y_MAX) ? 3'd6 :
-                      (((y_pos2 - Y_MIN) * 6) / Y_RANGE);
 
     ADC_reader adc0 (
         .clk(CLOCK_50),
@@ -59,14 +58,18 @@ module PONG_TOP(
         .ADC_CS_N(ADC_CS_N),
         .ADC_DIN(ADC_DIN),
         .ADC_SCLK(ADC_SCLK),
-        .y_pos1(y_pos1),
-        .y_pos2(y_pos2),
+        .pot0_raw(pot0_raw),
+        .pot1_raw(pot1_raw),
         .sample_strobe(sample_strobe)
     );
 
     game_engine game0 (
         .clk(CLOCK_50),
         .rst(KEY[0]),
+        .pot0_raw(pot0_raw),
+        .pot1_raw(pot1_raw),
+        .y_pos1(y_pos1),
+        .y_pos2(y_pos2),
         .ball_x(ball_x),
         .ball_y(ball_y),
         .score1(score1),
@@ -91,23 +94,43 @@ module PONG_TOP(
         .VGA_B(VGA_B)
     );
 
-    function [6:0] hex_bar;
-        input bottom_on;
-        input top_on;
+    // scale raw 0..4095 to decimal 0..999 for 3-digit display
+    assign pot0_dec = (pot0_raw * 10'd999) / 12'd4095;
+    assign pot1_dec = (pot1_raw * 10'd999) / 12'd4095;
+
+    assign pot0_hundreds = pot0_dec / 10'd100;
+    assign pot0_tens     = (pot0_dec % 10'd100) / 10'd10;
+    assign pot0_ones     = pot0_dec % 10'd10;
+
+    assign pot1_hundreds = pot1_dec / 10'd100;
+    assign pot1_tens     = (pot1_dec % 10'd100) / 10'd10;
+    assign pot1_ones     = pot1_dec % 10'd10;
+
+    function [6:0] seg7_decimal;
+        input [3:0] digit;
         begin
-            hex_bar = 7'b1111111;
-            if (top_on)
-                hex_bar[0] = 1'b0;
-            if (bottom_on)
-                hex_bar[3] = 1'b0;
+            case (digit)
+                4'd0: seg7_decimal = 7'b1000000;
+                4'd1: seg7_decimal = 7'b1111001;
+                4'd2: seg7_decimal = 7'b0100100;
+                4'd3: seg7_decimal = 7'b0110000;
+                4'd4: seg7_decimal = 7'b0011001;
+                4'd5: seg7_decimal = 7'b0010010;
+                4'd6: seg7_decimal = 7'b0000010;
+                4'd7: seg7_decimal = 7'b1111000;
+                4'd8: seg7_decimal = 7'b0000000;
+                4'd9: seg7_decimal = 7'b0010000;
+                default: seg7_decimal = 7'b1111111;
+            endcase
         end
     endfunction
 
-    assign HEX0 = hex_bar((p1_level >= 3'd1), (p2_level >= 3'd1));
-    assign HEX1 = hex_bar((p1_level >= 3'd2), (p2_level >= 3'd2));
-    assign HEX2 = hex_bar((p1_level >= 3'd3), (p2_level >= 3'd3));
-    assign HEX3 = hex_bar((p1_level >= 3'd4), (p2_level >= 3'd4));
-    assign HEX4 = hex_bar((p1_level >= 3'd5), (p2_level >= 3'd5));
-    assign HEX5 = hex_bar((p1_level >= 3'd6), (p2_level >= 3'd6));
+    assign HEX0 = seg7_decimal(pot0_ones);
+    assign HEX1 = seg7_decimal(pot0_tens);
+    assign HEX2 = seg7_decimal(pot0_hundreds);
+
+    assign HEX3 = seg7_decimal(pot1_ones);
+    assign HEX4 = seg7_decimal(pot1_tens);
+    assign HEX5 = seg7_decimal(pot1_hundreds);
 
 endmodule
