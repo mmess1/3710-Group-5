344e53903fffdf6f706d1fd391891a32937c2987
Meshal Alali <u1397679@utah.edu>
2026-04-10 16:28:54 -0600

    add 2 digit integration

--- 1_PROJECT_PONG/Video/renderer.v
+++ 1_PROJECT_PONG/Video/renderer.v
@@ -5,8 +5,8 @@ module renderer(
     input  wire [8:0] y_pos2,
     input  wire [9:0] ball_x,
     input  wire [9:0] ball_y,
-    input  wire [3:0] score1,
-    input  wire [3:0] score2,
+    input  wire [15:0] score1,
+    input  wire [15:0] score2,
     output wire       VGA_CLK,
     output wire       VGA_BLANK_N,
     output wire       VGA_VS,
@@ -26,46 +26,94 @@ module renderer(
     localparam PLAYER2_X     = 610;
     localparam BALL_SIZE     = 10;
 
-    localparam SCORE_Y    = 40;
-    localparam SCORE1_X   = 220;
-    localparam SCORE2_X   = 380;
+    localparam SCORE_Y          = 40;
+    localparam SCORE1_X_ONES    = 220;
+    localparam SCORE1_X_TENS    = SCORE1_X_ONES - 10 * 5;
+    localparam SCORE2_X_TENS    = 380;
+    localparam SCORE2_X_ONES    = (score2_tens < 0) ? (380 + 10 * 5) : 380;
+
     localparam FONT_SCALE = 8;
     localparam DIGIT_W    = 5 * FONT_SCALE;
     localparam DIGIT_H    = 5 * FONT_SCALE;
 
-    wire in_score1_box;
-    wire in_score2_box;
+    wire [3:0] score1_tens;
+    wire [3:0] score1_ones;
+    wire [3:0] score2_tens;
+    wire [3:0] score2_ones;
+
     wire [2:0] score1_row;
-    wire [2:0] score1_col;
+    wire [2:0] score1_col_ones;
+    wire [2:0] score1_col_tens;
     wire [2:0] score2_row;
-    wire [2:0] score2_col;
-    wire score1_on;
-    wire score2_on;
+    wire [2:0] score2_col_ones;
+    wire [2:0] score2_col_tens;
 
-    assign in_score1_box = (hcount >= SCORE1_X) && (hcount < SCORE1_X + DIGIT_W) &&
-                           (vcount >= SCORE_Y)  && (vcount < SCORE_Y + DIGIT_H);
+    wire in_score1_box_ones;
+    wire in_score1_box_tens;
+    wire in_score2_box_ones;
+    wire in_score2_box_tens;
 
-    assign in_score2_box = (hcount >= SCORE2_X) && (hcount < SCORE2_X + DIGIT_W) &&
-                           (vcount >= SCORE_Y)  && (vcount < SCORE_Y + DIGIT_H);
+    wire score1_ones_pixel;
+    wire score1_tens_pixel;
+    wire score2_ones_pixel;
+    wire score2_tens_pixel;
 
-    assign score1_row = (vcount - SCORE_Y) / FONT_SCALE;
-    assign score1_col = (hcount - SCORE1_X) / FONT_SCALE;
-    assign score2_row = (vcount - SCORE_Y) / FONT_SCALE;
-    assign score2_col = (hcount - SCORE2_X) / FONT_SCALE;
+    wire score1_tens_visible = (score1 >= 16'd10);
+    wire score2_tens_visible = (score2 >= 16'd10);
 
-    score_font left_digit (
-        .digit(score1),
+    assign score1_tens  = (score1 % 16'd100) / 16'd10;
+    assign score1_ones  = score1 % 16'd10;
+    assign score2_tens  = (score2 % 16'd100) / 16'd10;
+    assign score2_ones  = score2 % 16'd10;
+
+    assign in_score1_box_ones = (hcount >= SCORE1_X_ONES) && (hcount < SCORE1_X_ONES + DIGIT_W) &&
+                                (vcount >= SCORE_Y) && (vcount < SCORE_Y + DIGIT_H);
+    assign in_score1_box_tens = (hcount >= SCORE1_X_TENS) && (hcount < SCORE1_X_TENS + DIGIT_W) &&
+                                (vcount >= SCORE_Y) && (vcount < SCORE_Y + DIGIT_H);
+    assign in_score2_box_ones = (hcount >= SCORE2_X_ONES) && (hcount < SCORE2_X_ONES + DIGIT_W) &&
+                                (vcount >= SCORE_Y) && (vcount < SCORE_Y + DIGIT_H);
+    assign in_score2_box_tens = (hcount >= SCORE2_X_TENS) && (hcount < SCORE2_X_TENS + DIGIT_W) &&
+                                (vcount >= SCORE_Y) && (vcount < SCORE_Y + DIGIT_H);
+
+    assign score1_row       = (vcount - SCORE_Y) / FONT_SCALE;
+    assign score1_col_ones  = (hcount >= SCORE1_X_ONES) ? (hcount - SCORE1_X_ONES) / FONT_SCALE : 3'd0;
+    assign score1_col_tens  = (hcount >= SCORE1_X_TENS) ? (hcount - SCORE1_X_TENS) / FONT_SCALE : 3'd0;
+    assign score2_row       = (vcount - SCORE_Y) / FONT_SCALE;
+    assign score2_col_ones  = (hcount >= SCORE2_X_ONES) ? (hcount - SCORE2_X_ONES) / FONT_SCALE : 3'd0;
+    assign score2_col_tens  = (hcount >= SCORE2_X_TENS) ? (hcount - SCORE2_X_TENS) / FONT_SCALE : 3'd0;
+
+    score_font left_tens (
+        .digit(score1_tens),
         .row(score1_row),
-        .col(score1_col),
-        .pixel_on(score1_on)
+        .col(score1_col_tens),
+        .pixel_on(score1_tens_pixel)
     );
 
-    score_font right_digit (
-        .digit(score2),
-        .row(score2_row),
-        .col(score2_col),
-        .pixel_on(score2_on)
+    score_font left_ones (
+        .digit(score1_ones),
+        .row(score1_row),
+        .col(score1_col_ones),
+        .pixel_on(score1_ones_pixel)
     );
+
+    score_font right_tens (
+        .digit(score2_tens),
+        .row(score2_row),
+        .col(score2_col_tens),
+        .pixel_on(score2_tens_pixel)
+    );
+
+    score_font right_ones (
+        .digit(score2_ones),
+        .row(score2_row),
+        .col(score2_col_ones),
+        .pixel_on(score2_ones_pixel)
+    );
+
+    wire score1_on = (in_score1_box_tens && score1_tens_visible && score1_tens_pixel) ||
+                      (in_score1_box_ones && score1_ones_pixel);
+    wire score2_on = (in_score2_box_tens && score2_tens_visible && score2_tens_pixel) ||
+                      (in_score2_box_ones && score2_ones_pixel);
 
     clk_divider clk_div (
         .clk(CLOCK_50),
@@ -104,19 +152,19 @@ module renderer(
                     VGA_G <= 8'd255;
                     VGA_B <= 8'd255;
                 end
-                else if ((in_score1_box && score1_on) || (in_score2_box && score2_on)) begin
+                else if (score1_on || score2_on) begin
                     VGA_R <= 8'd255;
                     VGA_G <= 8'd255;
                     VGA_B <= 8'd255;
                 end
                 else if ((hcount >= PLAYER1_X) && (hcount < PLAYER1_X + PADDLE_WIDTH) &&
-                         (vcount >= y_pos1)    && (vcount < y_pos1 + PADDLE_HEIGHT)) begin
+                         (vcount >= y_pos1) && (vcount < y_pos1 + PADDLE_HEIGHT)) begin
                     VGA_R <= 8'd255;
                     VGA_G <= 8'd255;
                     VGA_B <= 8'd255;
                 end
                 else if ((hcount >= PLAYER2_X) && (hcount < PLAYER2_X + PADDLE_WIDTH) &&
-                         (vcount >= y_pos2)    && (vcount < y_pos2 + PADDLE_HEIGHT)) begin
+                         (vcount >= y_pos2) && (vcount < y_pos2 + PADDLE_HEIGHT)) begin
                     VGA_R <= 8'd255;
                     VGA_G <= 8'd255;
                     VGA_B <= 8'd255;
